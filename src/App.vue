330f60ffb083dd01e9cf180d7aba216e4e9c1ad2
aniciya777 <kisaost777@gmail.com>
2022-06-16 12:58:03 +0000

    First app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <MainHeaderVue />
+    <div class="wrapper-content">
+      <div class="container">
+        <p>
+          Ветклиника и ветаптека
+          «Друг рядом»
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MainHeaderVue from "@/components/MainHeader.vue";
+
+export default {
+  name: 'App',
+  components: {
+    MainHeaderVue,
+  }
+}
+</script>
+
+<style lang="scss">
+@font-face {
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+  url("@/assets/fonts/Montserrat-VariableFont_wght.ttf") format("truetype"),
+  url("@/assets/fonts/Montserrat-Italic-VariableFont_wght.ttf") format("truetype");
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="navbar">
+    <div class="container">
+      <div class="navbar-content">
+        <a class="navbar-logo" href="/">
+          <img src="@/assets/logo.png" alt="logo">
+        </a>
+        <ul class="navbar-list">
+          <li class="navbar-item" v-for="link in links" :key="link.alias">
+            <p :class="[{ 'navbar-link__in-button': link.in_button}]">
+              <a class="navbar-link" :href="link.url">
+                {{ link.title }}
+              </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import {app, links} from "@/_config";
+
+export default {
+  name: "MainHeader",
+  data () {
+    return {
+      title: app.title,
+      links,
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+  .navbar-link__in-button {
+    background-image: url("@/assets/svg/button_cabinet.svg");
+    background-size: contain;
+    padding: 8px 10px;
+  }
+</style>
