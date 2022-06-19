fcacf7207d1ffde3026fa26bc9b0656ceb70c5b0
aniciya777 <kisaost777@gmail.com>
2022-06-19 20:43:25 +0300

    Make 404 error page

--- src/components/pages/_errors/PageError_404.vue
+++ src/components/pages/_errors/PageError_404.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="container main-container">
-    <h1>
-      404 - Страница не найдена
-    </h1>
+    <div class="row page-error">
+      <div class="col-lg-5 col-sm-12">
+        <h1 class="error_code">404</h1>
+        <p class="m-0">
+          Ой, такой страницы нет!
+        </p>
+        <p>
+          Об этом явно кто-то позаботился :)
+        </p>
+        <p>
+          <router-link class="navbar-link" to="/">
+            <u>
+              Перейти на главную →
+            </u>
+          </router-link>
+        </p>
+      </div>
+      <div class="col-lg-7 col-sm-12">
+        <img src="@/assets/images/error_404.png" alt="404 - Page Not Found">
+      </div>
+    </div>
   </div>
 </template>
 
