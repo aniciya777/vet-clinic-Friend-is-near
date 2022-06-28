c1f018922a0bb0a702a95bafa41f68c1c0d17326
aniciya777 <kisaost777@gmail.com>
2022-06-29 01:37:28 +0300

    Add initial python API 4

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, Blueprint
 import os
-from waitress import serve
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='dist')
+
+client_bp = Blueprint("vue_js_bp", __name__, # 'Client Blueprint'
+    template_folder="dist", # Required for our purposes
+    static_folder="dist", # Again, this is required
+    static_url_path="/" # Flask will be confused if you don't do this
+)
 
 
-@app.route('/')
+@client_bp.route('/')
 def index():
     return render_template('index.html')
 
 
 @app.route('/api')
-def index():
+def api():
     return 'API'
 
 
 if __name__ == '__main__':
-    port_run = int(os.environ.get("PORT", 8080))
-    serve(app, host='0.0.0.0', port=port_run)
+    port_run = int(os.environ.get("PORT", 80))
+    app.register_blueprint(client_bp)
+    app.run(host='0.0.0.0', port=port_run)
